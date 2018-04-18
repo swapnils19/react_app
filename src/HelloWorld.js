90d4d72930209d523094ab2ca1770d6822ae3e4c
Swapnil Shende <shende.swapnil@promobitech.com>
2018-04-18 20:07:09 +0530

    1. Re-written HelloWorld component with `class`. 2. Added dynamically changing greeting functionality.

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './HelloWorld.css';
 
-const HelloWorld = props => {
-    return (<div className="HelloWorld">Hello {props.name}!</div>);
-};
+class HelloWorld extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { greeting: 'Hello' };
+        this.frenchify = this.frenchify.bind(this);
+    }
+
+    render() {
+        return (
+            <div className="HelloWorld">
+                {this.state.greeting} {this.props.name}!
+                <br />
+                <button onClick={this.frenchify}>Frenchify!</button>
+            </div>
+        );
+    }
+
+    frenchify() {
+        this.setState({ greeting: 'Bonjour' });
+    }
+}
 export default HelloWorld;
